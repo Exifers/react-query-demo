cf46bc47132448faf7918304549cd624b339edac
exifers <meric.emmanuel@gmail.com>
2021-08-17 17:18:14 +0200

    Add queryClientProvider

--- src/index.js
+++ src/index.js
@@ -1,47 +1,58 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 //
 
-import { Wrapper, Main } from './components/styled'
+import {Wrapper, Main} from './components/styled'
 import Sidebar from './components/Sidebar'
 
 import Admin from './screens/admin'
 import AdminEpisode from './screens/admin/Episode'
 import EpisodesList from './screens/episodes/EpisodesList'
 import EpisodeDetailCard from './screens/episodes/EpisodeDetailCard'
+import {QueryClientProvider, QueryClient} from "react-query";
 
-function SafeHydrate({ children }) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof document === 'undefined' ? null : children}
-    </div>
-  )
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        }
+    }
+})
+
+function SafeHydrate({children}) {
+    return (
+        <div suppressHydrationWarning>
+            {typeof document === 'undefined' ? null : children}
+        </div>
+    )
 }
 
 export default function App() {
-  return (
-    <SafeHydrate>
-      <BrowserRouter>
-        <Wrapper>
-          <Sidebar />
-          <Main>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <h1>Welcome!</h1>
-                  </>
-                }
-              />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/admin/:episodeId" element={<AdminEpisode />} />
-              <Route path="/episodes" element={<EpisodesList />} />
-              <Route path="/episodes/:episodeId" element={<EpisodeDetailCard />} />
-            </Routes>
-          </Main>
-        </Wrapper>
-      </BrowserRouter>
-    </SafeHydrate>
-  )
+    return (
+        <SafeHydrate>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Wrapper>
+                        <Sidebar/>
+                        <Main>
+                            <Routes>
+                                <Route
+                                    path="/"
+                                    element={
+                                        <>
+                                            <h1>Welcome!</h1>
+                                        </>
+                                    }
+                                />
+                                <Route path="/admin" element={<Admin/>}/>
+                                <Route path="/admin/:episodeId" element={<AdminEpisode/>}/>
+                                <Route path="/episodes" element={<EpisodesList/>}/>
+                                <Route path="/episodes/:episodeId" element={<EpisodeDetailCard/>}/>
+                            </Routes>
+                        </Main>
+                    </Wrapper>
+                </BrowserRouter>
+            </QueryClientProvider>
+        </SafeHydrate>
+    )
 }
